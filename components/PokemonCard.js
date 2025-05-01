bf4f21d6c539cca3bee448be0e634efd0bcf9ae8
Aylin Zarate <aylinzaratefernandez97@gmail.com>
2025-05-01 16:25:39 -0400

    feat: remove images

--- components/PokemonCard.js
+++ components/PokemonCard.js
@@ -3,17 +3,17 @@ var React = window.React; // Accedemos a React desde el global
 window.PokemonCard = React.createClass({
   render: function() {
     var types = this.props.pokemon.type.join(' / ');
-    var spritePath = './img/' + this.props.pokemon.sprite;
+    //var spritePath = './img/' + this.props.pokemon.sprite;
     
     return React.createElement('div', {className: 'pokemon-card'},
       
       React.createElement('h1', null, 'Content'),
       React.createElement('div', {className: 'pokemon-sprite'},
-        React.createElement('img', {
+     /*    React.createElement('img', {
           src: spritePath,
           alt: this.props.pokemon.name,
           style: {width: '64px', height: '64px'}
-        })
+        }) */
       ),
       React.createElement('div', {className: 'pokemon-info'},
         React.createElement('h2', null, this.props.pokemon.name),
