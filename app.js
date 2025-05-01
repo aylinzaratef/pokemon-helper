98eb1ebcfbf2b53c4124f1c5e9d46ecc9e141777
Aylin Zarate <aylinzaratefernandez97@gmail.com>
2025-05-01 16:58:22 -0400

    looking for the minimum functional structure

--- app.js
+++ app.js
@@ -1,23 +1,39 @@
-// Versión simple para probar compatibilidad
-var TestApp = React.createClass({
-    getInitialState: function() {
-      return { clicks: 0 };
-    },
-    
-    handleClick: function() {
-      this.setState({ clicks: this.state.clicks + 1 });
-    },
-    
+// Datos de los Pokémon
+var pokemons = [
+    { id: 1, name: 'Bulbasaur', type: 'Planta/Veneno' },
+    { id: 4, name: 'Charmander', type: 'Fuego' },
+    { id: 7, name: 'Squirtle', type: 'Agua' }
+  ];
+  
+  // Componente de la tabla de Pokémon
+  var PokemonTable = React.createClass({
     render: function() {
-      return React.createElement('div', null,
-        React.createElement('h1', null, 'Prueba React ' + React.version),
-        React.createElement('p', null, 'Clics: ' + this.state.clicks),
-        React.createElement('button', { onClick: this.handleClick }, 'Haz clic')
+      return React.createElement('div', {className: 'app'},
+        React.createElement('h1', null, 'Pokémon Iniciales Gen 1'),
+        React.createElement('table', null,
+          React.createElement('thead', null,
+            React.createElement('tr', null,
+              React.createElement('th', null, 'ID'),
+              React.createElement('th', null, 'Nombre'),
+              React.createElement('th', null, 'Tipo')
+            )
+          ),
+          React.createElement('tbody', null,
+            this.props.pokemons.map(function(pokemon) {
+              return React.createElement('tr', {key: pokemon.id},
+                React.createElement('td', null, pokemon.id),
+                React.createElement('td', null, pokemon.name),
+                React.createElement('td', null, pokemon.type)
+              );
+            })
+          )
+        )
       );
     }
   });
   
+  // Renderizar la aplicación
   ReactDOM.render(
-    React.createElement(TestApp),
+    React.createElement(PokemonTable, {pokemons: pokemons}),
     document.getElementById('root')
   );
