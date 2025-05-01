9cf829bf28a50eb0209ad741ed9649b4ee14e069
Aylin Zarate <aylinzaratefernandez97@gmail.com>
2025-05-01 13:02:47 -0400

    feat: project initialization

---
+++ pokemon-helper/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+     
+        <p>
+          3DS Test
+        </p>
+       page with React.js
+      </header>
+    </div>
+  );
+}
+
+export default App;
