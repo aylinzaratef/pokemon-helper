eb85ff32bab04e4775db0b778baf3ddd8bef8b85
Aylin Zarate <aylinzaratefernandez97@gmail.com>
2025-05-01 14:21:44 -0400

    feat: test on 3ds

--- README.md
+++
@@ -1,2 +0,0 @@
-# pokemon-helper
-Designed to help new players who decide to venture into the world of Pokémon.  Initially focused on games from generation 1 to 7 on Nintendo 3DS.Designed to help new players who decide to venture into the world of Pokémon.  Initially focused on games from generation 1 to 7 on Nintendo 3DS.
---
+++ app.js
@@ -0,0 +1,43 @@
+// Lista de Pokémon de ejemplo
+var pokemonData = [
+    { id: 1, name: 'Bulbasaur', type: 'Grass/Poison' },
+    { id: 2, name: 'Charmander', type: 'Fire' },
+    { id: 3, name: 'Squirtle', type: 'Water' }
+  ];
+  
+  // Componente PokemonCard sin JSX
+  var PokemonCard = React.createClass({
+    render: function() {
+      return React.DOM.div(
+        { className: 'pokemon-card' },
+        React.DOM.h2(null, this.props.pokemon.name),
+        React.DOM.p(null, 'Tipo: ' + this.props.pokemon.type),
+        React.DOM.p(null, 'ID: ' + this.props.pokemon.id)
+      );
+    }
+  });
+  
+  // Componente principal App sin JSX
+  var App = React.createClass({
+    render: function() {
+      return React.DOM.div(
+        { className: 'app' },
+        React.DOM.h1(null, 'Pokédex 3DS'),
+        React.DOM.div(
+          { className: 'pokemon-list' },
+          this.props.pokemons.map(function(pokemon) {
+            return React.createElement(PokemonCard, { 
+              key: pokemon.id, 
+              pokemon: pokemon 
+            });
+          })
+        )
+      );
+    }
+  });
+  
+  // Renderizar la aplicación
+  ReactDOM.render(
+    React.createElement(App, { pokemons: pokemonData }),
+    document.getElementById('root')
+  );
